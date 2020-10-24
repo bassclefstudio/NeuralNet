5b7f7966812854947b62599d7bd43ce519005fbd
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-10-24 12:10:05 -0400

    Added XML documentation for all new items and updated all documentation for changes in namespace.

--- BassClefStudio.NeuralNet.Core/Networks/Layer.cs
+++ BassClefStudio.NeuralNet.Core/Networks/Layer.cs
@@ -5,11 +5,30 @@ using System.Text;
 
 namespace BassClefStudio.NeuralNet.Core.Networks
 {
+    /// <summary>
+    /// Represents a group of <see cref="Neuron"/>s in a given layer.
+    /// </summary>
     public class Layer
     {
+        /// <summary>
+        /// Gets the <see cref="Neuron"/> in this <see cref="Layer"/> at the specified <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">The index of the <see cref="Neuron"/> in this <see cref="Layer"/>'s <see cref="Neurons"/>.</param>
+        public Neuron this[int index]
+        { 
+            get => Neurons[index];
+            set => Neurons[index] = value;
+        }
+
+        /// <summary>
+        /// The collection of <see cref="Neurons"/> in this <see cref="Layer"/>.
+        /// </summary>
         public Neuron[] Neurons { get; set; }
 
-        public Layer(IEnumerable<Neuron> neurons) : this(neurons.ToArray()) { }
+        /// <summary>
+        /// Creates a new <see cref="Layer"/> from an array of <see cref="Neuron"/>s.
+        /// </summary>
+        /// <param name="neurons">The <see cref="Neuron"/>s in this array.</param>
         public Layer(Neuron[] neurons)
         {
             Neurons = neurons;
--- BassClefStudio.NeuralNet.Core/Networks/Neuron.cs
+++ BassClefStudio.NeuralNet.Core/Networks/Neuron.cs
@@ -6,16 +6,33 @@ using System.Text;
 
 namespace BassClefStudio.NeuralNet.Core.Networks
 {
+    /// <summary>
+    /// Represents a single <see cref="Neuron"/> in a <see cref="NeuralNetwork"/> that speicifies how a single feed-forward operation should function.
+    /// </summary>
     public class Neuron
     {
+        /// <summary>
+        /// The current activation value of this <see cref="Neuron"/>.
+        /// </summary>
         public double Activation { get; set; }
 
+        /// <summary>
+        /// The bias of this <see cref="Neuron"/>.
+        /// </summary>
         public double Bias { get; set; }
 
+        /// <summary>
+        /// An array of <see cref="Synapse"/> values describing how this <see cref="Neuron"/> should be connected to <see cref="Neuron"/>s in the previous layer.
+        /// </summary>
         public Synapse[] Synapses { get; set; }
 
-        public Neuron()
+        /// <summary>
+        /// Creates a new <see cref="Neuron"/>.
+        /// </summary>
+        /// <param name="activation">The initial <see cref="Activation"/> value. Defaults to 0.</param>
+        public Neuron(double activation = 0)
         {
+            Activation = activation;
         }
     }
 }
